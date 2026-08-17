fe39ec97fed865e7b065acc96738a37e47fb47bc
Ajit Soni <ajitsoni000005@gmail.com>
2026-08-17 14:21:00 +0530

    fix: remove unused function

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -75,10 +75,6 @@ impl MhtmlArchive {
     pub fn resource_url(&self, index: usize) -> Option<String> {
         self.0.get_index(index).and_then(|r| r.url.clone())
     }
-    
-    pub fn resource_bytes_len(&self, index: usize) -> Option<usize> {
-        self.0.get_index(index).map(|r| r.body.len())
-    }
 
     pub fn resource_charset(&self, index: usize) -> Option<String> {
         self.0.get_index(index).and_then(|r| r.charset.clone())
